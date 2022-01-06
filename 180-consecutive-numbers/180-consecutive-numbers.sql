67aa2eea26ffb69ff541d5748c934ddff43a409d
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2022-01-07 01:16:03 +0530

    Time: 463 ms (54.76%), Space: 0B (100.00%) - LeetHub

---
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -0,0 +1,9 @@
+select distinct a.num as ConsecutiveNums 
+from Logs a
+inner join Logs b 
+    on b.id = a.id + 1 
+    and b.num = a.num
+inner join Logs c 
+    on c.id = a.id + 2 
+    and c.num = a.num
+
