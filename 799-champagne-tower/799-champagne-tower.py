7d4b2b9bc444fe1dafa30019d2144c83e30eee28
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2022-03-12 19:02:03 +0530

    Time: 104 ms (87.98%), Space: 14.2 MB (20.32%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.py
+++ 799-champagne-tower/799-champagne-tower.py
@@ -1,10 +1,10 @@
 class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
-        glass = [[0] * k for k in range(1 , 102)]
+        glass = [[0] * k for k in iter(range(1 , 102))]
         glass[0][0] = poured
         
-        for row in range(query_row + 1):
-            for col in range(row + 1):
+        for row in iter(range(query_row + 1)):
+            for col in iter(range(row + 1)):
                 quantity = (glass[row][col] - 1.0) / 2.0
                 if quantity > 0:
                     glass[row + 1][col] += quantity
